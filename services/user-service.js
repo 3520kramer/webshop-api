9276c90d6538e1e24ad6a2c033b44da989186b57
Oliver Kramer <3520_kramer@live.dk>
2021-03-25 11:21:15 +0100

    added autoIncrement to model and started creating user-service

--- services/user-service.js
+++ services/user-service.js
@@ -3,11 +3,9 @@ const userModel = require('../models/user');
 
 const createUser = async (username, password, createdDate) => {
     console.log(username, password, createdDate)
-    let user = await userModel.create({ userID: null, username: username, password: password, createdDate: createdDate }).then(res => console.log("HEY", res, userModel)).catch(error => console.log("ERROR", error))
-    
-} 
+    let user = await userModel.create({ userID: null, username: username, password: password, createdDate: createdDate }).then(res => console.log(res)).catch(error => console.log("ERROR", error))
+};
 
 module.exports = {
     createUser,
-
 }
