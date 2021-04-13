1966f27904e3cf804e9db9e4ee42ec32892a6cbe
MadsRF <madsrunefrederiksen@gmail.com>
2021-04-14 00:10:52 +0200

    fix

--- services/order-service.js
+++ services/order-service.js
@@ -5,15 +5,15 @@ const orderModel = require('../models/orders')(sequelize, DataTypes);
 
 // works with transaction
 const createOrder = async (req, res) => {
-    console.log("createOrder", req.body);
-    let input = req.body;
+  console.log("createOrder", req.body);
+  let input = req.body;
 
-    console.log("input", input);
-    try {
-      await sequelize.transaction(async (t) => {
+  console.log("input", input);
+  try {
+    await sequelize.transaction(async (t) => {
       const order = await orderModel.create({
-        order_id: null, 
-        created: input.created, 
+        order_id: null,
+        created: input.created,
         comment: input.comment,
         shipped_date: input.shipped_date,
         customers_customer_id_billing: input.customer_id_billing,
@@ -22,33 +22,33 @@ const createOrder = async (req, res) => {
         employees_employee_id: input.employee_id,
         po_boxes_id: input.po_boxes_id,
         warehouses_warehouse_id: input.warehouse_id
-      }, { transaction: t },);
+      }, { transaction: t });
 
       console.log("order", order);
       if (order) {
-        return res.status(201).json({order});
+        return res.status(201).json({ order });
       }
 
     })
-    } catch (error) {
-      return res.status(500).json({error: error.message})
-    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
   }
+}
 
 // works. should perhaps have a try/catch
 const getOneOrder = async (req, res) => {
   console.log("getOneOrder", req.query);
   let id = req.query.id;
-  orderModel.findOne({where: {order_id: id}}).then( (orderModel) => {
-      console.log(orderModel);
-      res.send(orderModel);
-  }, );
+  orderModel.findOne({ where: { order_id: id } }).then((orderModel) => {
+    console.log(orderModel);
+    res.send(orderModel);
+  });
 }
 
 // works with pagination
 const getAllOrders = async (req, res) => {
   console.log("getAllOrders", req.query);
-  
+
   // taking input and parsing to int 
   const pageAsNumber = Number.parseInt(req.query.page);
   const sizeAsNumber = Number.parseInt(req.query.size);
@@ -62,24 +62,24 @@ const getAllOrders = async (req, res) => {
   // default response size of rows
   let size = 1000;
   if (!Number.isNaN(sizeAsNumber) && sizeAsNumber > 0 && sizeAsNumber < 100000) {
-      size = sizeAsNumber;
+    size = sizeAsNumber;
   }
-  
-  try {
-  const orders = await orderModel.findAndCountAll({
-    limit: size,
-    offset: page * size
-  });
 
-  return res.send({
-    total: orders.count,
-    content: orders.rows,
-    // rounds off the total of pages 
-    totalPages: Math.ceil(orders.count / size)
-  });
+  try {
+    const orders = await orderModel.findAndCountAll({
+      limit: size,
+      offset: page * size
+    });
+
+    return res.send({
+      total: orders.count,
+      content: orders.rows,
+      // rounds off the total of pages 
+      totalPages: Math.ceil(orders.count / size)
+    });
 
   } catch (error) {
-    return res.status(500).json({error: error.message})
+    return res.status(500).json({ error: error.message })
   }
 }
 
@@ -90,7 +90,7 @@ const searchOrders = async (req, res) => {
   const id = req.query.id;
 
   const pageAsNumber = Number.parseInt(req.query.page);
-  
+
   // default size 
   const size = 1000;
 
@@ -99,34 +99,35 @@ const searchOrders = async (req, res) => {
   if (!Number.isNaN(pageAsNumber) && pageAsNumber > 0) {
     page = pageAsNumber;
   }
-  
+
   try {
     const orders = await orderModel.findAndCountAll({
       where: {
         [search]: {
           [Op.like]: `%${id}%`
-        }},
+        }
+      },
       limit: size,
       offset: page * size
 
     });
-  
+
     return res.send({
       total: orders.count,
       content: orders.rows,
       // rounds off the total of pages 
       totalPages: Math.ceil(orders.count / size)
     });
-  
-    } catch (error) {
-      return res.status(500).json({error: error.message})
-    }
+
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
 }
-  
+
 
 module.exports = {
-    createOrder,
-    getOneOrder,
-    getAllOrders,
-    searchOrders,
+  createOrder,
+  getOneOrder,
+  getAllOrders,
+  searchOrders,
 }
