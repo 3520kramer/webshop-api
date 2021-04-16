67e33ff6d794e9701cf601e6d9376e3856f1ea60
MadsRF <madsrunefrederiksen@gmail.com>
2021-04-16 12:23:10 +0200

    wokring login/out. needs cleanup

--- app.js
+++ app.js
@@ -1,16 +1,50 @@
 const express = require("express");
 const app = express();
 
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+// Used to be able to establish permission for the user to enter the site and also be able to track them
+const session = require("express-session");
+const config = require("./configuration/config.json");
+app.use(session({
+    secret: config.sessionSecret,
+    resave: false,
+    saveUninitialized: true
+}));
+
+
+// Function for checking session id on routes request 
+const checkAuth = (req, res, next) => {
+    if (req.session.userId != config.sessionSecret)  {
+      return res.status(401).send({ response: "not allowed" });
+    } else {
+        console.log('checkAuth logged in', req.session.userId + " = " + config.sessionSecret);
+        next();
+    };
+};
+
+
+// get routes. These have to be after the checkAuth function or else it can't find req.session.userId
 const userRoutes = require('./routes/user-routes');
 const productRoutes = require('./routes/product-routes');
 const orderRoutes = require('./routes/order-routes');
+const loginRoutes = require('./routes/login-routes');
 
+
+// use routes 
 app.use(userRoutes);
 app.use(productRoutes);
 app.use(orderRoutes);
+app.use(loginRoutes);
+
+
+// test route for now with checkAuth
+app.get("/test", checkAuth, (req, res) => {
+    return res.status(200).send({ response: "hallo this is the secret route" });
+});
 
 
 const port = process.env.PORT ? process.env.PORT : 6000;
---
+++ configuration/bcrypt.js
@@ -0,0 +1,5 @@
+// hashed value of: 203162548ba1ecdcb78ecbc56603fac9
+// $2b$12$sFX4bN3TlD7OXjuGWYYuxOPcCpw2C1BZn2.mDMj2IBMbxSqZjg94q
+/*bcrypt.hash(password, saltRounds).then(data => {
+    console.log("data", data);
+})*/
