c6e162220c0721028fc3d34062c4ee4210f9b793
3520kramer <43636461+3520kramer@users.noreply.github.com>
2021-04-20 20:54:09 +0200

    Kramer/employee login, authorization, roles, register user, update user login (#11)
    * Updated checkAuth to look at the role of the user, updated login for user, started login for employee
    
    * Added register user and login for employee, finished checkauth

--- services/login-service.js
+++ services/login-service.js
@@ -1,58 +1,84 @@
-const DataTypes = require("sequelize").DataTypes;
-const sequelize = require('../database/connect').database;
-const User = require('../models/users')(sequelize, DataTypes);
-
+const models = require('../database/connect').models;
+const { role } = require('../routes/route-authorization');
+const userService = require('./user-service');
 
 // Connects to the session secret  
-const sessionId = require("../configuration/config.json");
-
+const { sessionSecret } = require("../configuration/config.json");
 
 // A library to help you hash passwords.
 const bcrypt = require('bcrypt');
+
 // When you are hashing your data the module will go through a series of rounds to give you a secure hash.
 const saltRounds = 12;
 
 
 // works
-const login = async (req, res) => {
+const loginUser = async (username, password) => {
+    try {
+        const user = await models.users.findOne({ where: { username: username } });
 
-    const { username, password } = req.body;
-    console.log(req.body);
+        if (!user) throw new Error("Something went wrong when getting information from database");
 
-    if (username && password) {
+        const result = await bcrypt.compare(password, user.password);
 
-        User.findOne({ where: { username: username } }).then((foundUser) => {
-            try {
-                bcrypt.compare(password, foundUser.password).then(result => {
-                    //console.log("result", result);
-                    if (result == true) {
-                        req.session.userId = sessionId.sessionSecret;
-                        req.session.username = foundUser.username;
-                        req.session.password = password;
-                        return res.status(200).send({ response: req.session.username + " - logged in" });
-                    } else {
-                        return res.status(401).send({ response: "username or password incorrect, try again" });
-                    };
-                });
-            } catch (error) {
-                return res.status(400).send({ response: "Something went wrong when getting information from database" });
-            }
-        })
-    };
+        if (!result) throw new Error("username or password incorrect, try again");
+
+        return {
+            sessionSecret: sessionSecret.user,
+            userId: user.user_id,
+            username: user.username
+        }
+    } catch (error) {
+        return { error: error.message };
+    }
 };
 
+const loginEmployee = async (email, password) => {
+    try {
+        const employee = await models.employees.findOne({ where: { email: email } });
+
+        if (!employee) throw new Error("Something went wrong when getting information from database");
+
+        const result = await bcrypt.compare(password, employee.password);
+
+        if (!result) throw new Error("email or password incorrect, try again");
+
+        let _sessionSecret = employee.job_title.toLowerCase() === role.ADMIN ? sessionSecret.admin : sessionSecret.employee
+
+        return {
+            sessionSecret: _sessionSecret,
+            employeeId: employee.employeeId,
+            email: employee.email,
+        }
+
+    } catch (error) {
+        return { error: error.message };
+    }
+};
+
+const registerUser = async (newUser) => {
+    try{
+        let hashedPassword = await bcrypt.hash(newUser.password, saltRounds);
+
+        newUser.password = hashedPassword
+
+        const createdUser = await userService.createUser(newUser);
+        
+        return createdUser;
+
+    } catch (error) {
+        return { error: error.message };
+    }
+};
 
 // works
-const logout = async (req, res) => {
-    console.log("logout");
-    req.session.destroy((err) => {
-        return res.status(200).send({ response: "logout" });
-    });
+const hashTest = async (req, res) => {
+    bcrypt.hash(req.query.pw, saltRounds).then(result => res.send(result));
 };
 
-
 module.exports = {
-    login,
-    //signup,
-    logout
+    loginUser,
+    loginEmployee,
+    registerUser,
+    hashTest,
 }
