16a8e11af6089bfdb19f27ad79c2ac0d5b4f0e7e
Oliver Kramer <3520_kramer@live.dk>
2021-05-15 15:36:47 +0200

    changed mongo service to actually mongo

--- services/mongodb/product-service.js
+++ services/mongodb/product-service.js
@@ -5,8 +5,10 @@ const getAllProducts = async () => {
         console.log("IN SERVICE LAYER GETALLPRODUCTS");
         const product = await Product.find({});
         
+        //const product = {Test: "TEST FROM SERVICE LAYER"};
+
         console.log("product", product);
-        
+
         return product;
     }catch(error){
         console.log(error);
