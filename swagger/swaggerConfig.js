17696313b08878504522afe69fe918da7def9f5a
Mads R. Frederiksen <madsrunefrederiksen@gmail.com>
2021-05-19 18:53:57 +0200

    Feature/mongoproduct (#42)
    * worked on mongo products
    
    * init

--- swagger/swaggerConfig.js
+++ swagger/swaggerConfig.js
@@ -8,6 +8,7 @@ const httpType = process.env.NODE_ENV === 'production' ? 'https' : 'http';
 console.log("environment", process.env.NODE_ENV);
 console.log("httpType", httpType);
 
+
 // Swagger definition
 const swaggerDefinitionMongo = {
     // info: General information about the API, such as: version, title and description.
@@ -46,14 +47,19 @@ const swaggerFileMongo = swaggerJSDoc({swaggerDefinition: swaggerDefinitionMongo
 router.use('/swagger', swaggerUi.serve, swaggerUi.setup(null, options));
 
 router.get('/swagger-sql-json', (req, res) => {
+    console.log("environment", process.env.NODE_ENV);
+    console.log("httpType", httpType);
+    
     config.isMongoUsed = false;
-
     MongoDb.closeMongoConnection();
 
     res.json(swaggerFileSql);
 });
 
 router.get('/swagger-mongo-json', (req, res) => {
+    console.log("environment", process.env.NODE_ENV);
+    console.log("httpType", httpType);
+    
     config.isMongoUsed = true;
     
     // Connects to mongo
