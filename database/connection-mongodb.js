71584b0bbc5173c6f785a588bdcc77371f7485e5
3520kramer <43636461+3520kramer@users.noreply.github.com>
2021-05-25 19:10:32 +0200

    Feature/login mongo (#50)
    * changed swagger url to /swagger
    
    * Updated database roles, implemented datawarehouse-ish
    
    * Updated connect with models
    
    * Added role description in routes and changed some authorization
    
    * Changed product model types to required and implemented pagination on get all orders-mongo
    
    * Finished search on orders/mongo
    
    * Updated swagger docs for Ordersearch and getAllOrders
    
    * Started user/priviliges/login against other mongo cluster
    
    * Finished login
    
    * updated env.template
    
    * changed default user in mongodocs to admin

--- database/connection-mongodb.js
+++ database/connection-mongodb.js
@@ -1,32 +1,58 @@
 const config = require("../configuration/config");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const createMongoConnection = () => {
-    try{
-        mongoose.connect(process.env.MONGO_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
-        const db = mongoose.connection;
-        
-        db.on('error', console.error.bind(console, 'connection error:'));
-        
-        db.once('open', () => {
-          // we're connected!
-          console.log('MONGO: Connection opened');
-        });
+  try {
+    mongoose.connect(config.mongoConnectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const db = mongoose.connection;
 
-    }catch(error){
-        console.log(error);
-    }
-}
+    db.on("error", console.error.bind(console, "connection error:"));
+
+    db.once("open", () => {
+      // we're connected!
+      console.log("MONGO: Connection opened");
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const closeMongoConnection = () => {
-    try{
-        mongoose.disconnect().then(() => console.log('MONGO: Connection closed'));
-    }catch(error){
-        console.log(error);
+  try {
+    mongoose.disconnect().then(() => console.log("MONGO: Connection closed"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateMongoConnection = async (role) => {
+  try {
+    const user = config.databaseSecretMongo[role].user;
+    const password = config.databaseSecretMongo[role].password;
+    const databaseName = config.databaseName;
+
+    const newConnectionString = `mongodb+srv://${user}:${password}@cluster0.tfmsf.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
+    console.log("updateMongoConnection");
+
+    if (config.mongoConnectionString !== newConnectionString) {
+      config.mongoConnectionString = newConnectionString;
+      closeMongoConnection();
+      createMongoConnection();
+    } else {
+      console.log("MONGO CONNECTION KEPT OPEN");
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 // used in app.js to initialize connection
 module.exports = {
-    createMongoConnection,
-    closeMongoConnection
-}
+  createMongoConnection,
+  closeMongoConnection,
+  updateMongoConnection,
+};
